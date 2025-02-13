2862fdad5e5639f9982df3b59e87bc2d6b530c4d
Veronica <no.demchenko@yandex.ru>
2025-02-13 14:50:14 +0300

    refactor: add comments and moving methods/imports

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import os
-import shutil
-from os import listdir
-from os.path import isfile
-
-from dotenv import load_dotenv, dotenv_values
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.dispatcher.filters import Text, BoundFilter
+from dotenv import load_dotenv
+
+import os
+from os import listdir
 from os.path import isfile, join
-from aiogram.types import ContentType
 
 
 # загрузка переменных окружения
@@ -17,17 +13,18 @@ API_TOKEN = os.getenv("MY_TOKEN")
 bot = Bot(token=API_TOKEN)
 dispatcher = Dispatcher(bot)
 
+# Папка для сохранения изображений
 DOWNLOADS_FOLDER = 'downloads'
-
-
-class IsPrivate(BoundFilter):
-    async def check(self, message: types.Message):
-        return types.ChatType.PRIVATE == message.chat.type
 
 
 @dispatcher.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     await message.answer('Привет! Пришли мне список фото и я их перемешаю')
+
+
+@dispatcher.message_handler(content_types=types.ContentTypes.PHOTO)
+async def handle_photo(message: types.Message):
+    await download_photo(message)
 
 
 async def download_photo(message: types.Message):
@@ -47,17 +44,12 @@ async def download_photo(message: types.Message):
         await photo_file.download(photo_path)
 
         # Далее можно провести необходимую обработку или отправить ответ пользователю
-        await shuffle_photos(message)
+        await mix_up_photos(message)
 
 
-async def shuffle_photos(message):
+async def mix_up_photos(message):
     images = [f for f in listdir(DOWNLOADS_FOLDER) if isfile(join(DOWNLOADS_FOLDER, f))]
     print(images)
-
-
-@dispatcher.message_handler(content_types=types.ContentTypes.PHOTO)
-async def handle_photo(message: types.Message):
-    await download_photo(message)
 
 
 executor.start_polling(dispatcher)
